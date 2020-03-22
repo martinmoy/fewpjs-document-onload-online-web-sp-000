3d58fbab85adaf2f247042aa9d8acb80a4a8eb87
Martin Moy <martinmoy1991@gmail.com>
2020-03-22 14:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
-text = document.getElementById("text").innerHTML = "This is really cool!"
 
-document.addEventListener("DOMContentLoaded", function() { text});
+
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("text").innerHTML = "This is really cool!"
+});
